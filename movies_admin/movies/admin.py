001bfd9b41c13e1a63be80032ee32dced3a61112
Dmitry Morozov <adm713934>
2024-03-24 21:24:31 +0300

    [in process] 2/3 done

---
+++ movies_admin/movies/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Filmwork, Genre, GenreFilmwork, Person
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ('full_name',)
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
+    list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
